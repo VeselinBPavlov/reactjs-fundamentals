8b3a74351af3205d0285f4e25e266bfecf85c6c4
Veselin Pavlov <veselin.b.pavlov@gmail.com>
2019-10-29 13:09:20 +0200

    Updated React components lab.

--- 03. React Components - Lab/src/App.js
+++ 03. React Components - Lab/src/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import Wellcome from './components/Welcome';
-import Timer from './components/Timer';
+import React from 'react';
 import './App.css';
+import Welcome from './components/Welcome';
+import Timer from './components/Timer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Wellcome title ='Hello from React.' subtitle = 'Enjoy building your apps.' />
-        <Timer />
-      </div>
-    );
-  }
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome title ='Hello from React.' subtitle = 'Enjoy building your apps.' />
+      <Timer />
+    </div>
+  );
 }
 
 export default App;
--- 03. React Components - Lab/src/components/Logo.js
+++ 03. React Components - Lab/src/components/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from '../logo.svg';
 
 class Logo extends React.Component {
     render() {
         return (
-            <span>Logo</span>
+            <img src={logo} className="App-logo" alt="logo" />
         )
     }
 }
